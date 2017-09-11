331b1c87307326c24339c0dc1623ca1c3a5b2f16
coel16 <modopojken@gmail.com>
2017-09-11 11:13:45 +0200

    uppdaterad kod

--- programering/Första programmet/test.java
+++ programering/Första programmet/test.java
@@ -1,8 +1,15 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
     
     public class test {
         public static void main(String[] args) {
-            JOptionPane.showMessageDialog(null, "Hello World");
-            JOptionPane.showMessageDialog(null, "slick AF");
+            //JOptionPane.showMessageDialog(null, "Hej!" + " Dabba really hard my dudddddduh!" + "\nYayeYA YEyaah" + "\nJag har ett par riktigt fina linebreaks!");
+            //Toolkit.getDefaultToolkit().beep();
+            //System.getProperty("user.name");
+            JOptionPane.showMessageDialog(null, "Info", "Information" JOptionPane.INFORMATION_MESSAGE);
+            //System.getProperties().list(System.out);
+            //JOptionPane.showMessageDialog(null, "Datum och Tid: \n" + Calendar.getInstance().getTime().toString());
+            
         }
     }
