af1d48db718b16160243928a7f417a342e011e4b
coel16 <modopojken@gmail.com>
2017-09-11 09:48:41 +0200

    kommentar

---
+++ programering/Första programmet/test.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+    
+    public class test {
+        public static void main(String[] args) {
+            JOptionPane.showMessageDialog(null, "Hello World");
+            JOptionPane.showMessageDialog(null, "slick AF");
+        }
+    }
